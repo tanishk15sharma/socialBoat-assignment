7cbf6bdf96ade75871c459e49889086074a41fdc
tanishk15sharma <tanishk1582002@gmail.com>
2022-06-22 00:23:40 +0530

    initial setup

---
+++ socialboat/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Header } from "./components/Header";
+import { VideoCard } from "./components/VideoCard";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <VideoCard />
+    </div>
+  );
+}
+
+export default App;
