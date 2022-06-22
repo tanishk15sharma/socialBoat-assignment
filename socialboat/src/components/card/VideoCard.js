09a4c3649b45f7a48c120323c4b8c6bc843baa9f
tanishk15sharma <tanishk1582002@gmail.com>
2022-06-23 01:10:30 +0530

    style:video card , responsive videos list

--- socialboat/src/components/card/VideoCard.js
+++ socialboat/src/components/card/VideoCard.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { useVideos } from "../../contexts/videos";
+import "./VideoCard.css";
 
 const VideoCard = ({ video }) => {
   console.log(video);
   return (
-    <div>
+    <div className="videocard">
       <iframe src={video.video}></iframe>
+      <div className="video-info">
+        <span className="video-heading">{video.heading}</span>
+        <ul className="video-tags">
+          {video.tags.map((tag) => (
+            <li className="tag">{tag}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
